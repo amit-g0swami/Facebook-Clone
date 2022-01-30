da429c0fb90191379f83d91e15f65f77ec67528f
seekamentor-dev <98406096+seekamentor-dev@users.noreply.github.com>
2022-01-30 16:20:30 +0530

    Header added

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBKmVhjTDuP8FRKc308OQIS4SmouEYbtuE",
+  authDomain: "facebook-clone-610da.firebaseapp.com",
+  projectId: "facebook-clone-610da",
+  storageBucket: "facebook-clone-610da.appspot.com",
+  messagingSenderId: "888574990662",
+  appId: "1:888574990662:web:df6d7d25c3d704d1576d47",
+  measurementId: "G-VFLKYLPYPF",
+};
