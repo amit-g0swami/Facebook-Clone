0ae37b5ac3f1b92a6d0fae648992654af79ba480
seekamentor-dev <98406096+seekamentor-dev@users.noreply.github.com>
2022-02-02 14:57:28 +0530

    storyreels added

---
+++ src/components/StoryReel.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Story from "./Story";
+import "./StoryReel.css";
+
+function StoryReel() {
+  return (
+    <div className="storyreel">
+      <Story
+        image="https://i.pinimg.com/originals/b7/1e/41/b71e4103a7245079c6d4f8c47ac0f932.jpg"
+        profileSrc="https://www.gravatar.com/avatar/1b8fabaa8d66250a7049bdb9ecf44397?s=250&d=mm&r=x"
+        title="King Pin"
+      />
+      <Story
+        image="https://theworldpursuit.com/wp-content/uploads/2020/04/Bali-.jpeg"
+        profileSrc="https://i.pinimg.com/originals/de/64/80/de64801f0275c1ab2ea5a9e2bb3ce7bc.jpg"
+        title="Sonny sangah"
+      />
+      <Story
+        image="https://awesoroo.com/wp-content/uploads/2018/08/sunset-red-color-mobile.jpg"
+        profileSrc="https://media.istockphoto.com/photos/young-handsome-man-with-beard-wearing-casual-sweater-standing-over-picture-id1212702108?k=20&m=1212702108&s=612x612&w=0&h=ZI4gKJi2d1dfi74yTljf4YhulA1nfhD3dcUFGH-NUkY="
+        title="amit g0swami"
+      />
+      <Story
+        image="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/9a5b7998-a0a7-4146-b0ef-43d52173f718/d5kuo39-a73147e6-345f-443d-889e-a3b16b92478d.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzlhNWI3OTk4LWEwYTctNDE0Ni1iMGVmLTQzZDUyMTczZjcxOFwvZDVrdW8zOS1hNzMxNDdlNi0zNDVmLTQ0M2QtODg5ZS1hM2IxNmI5MjQ3OGQuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.GbK1opB0Nk5tgh6z1K3Gnnuebssax6s1o8gySXT_lPU"
+        profileSrc="https://i.pinimg.com/originals/ae/ec/c2/aeecc22a67dac7987a80ac0724658493.jpg"
+        title="Vladimer lenin"
+      />
+      <Story
+        image="https://onetwostream.com/blog/wp-content/uploads/2019/10/sunset_beach.jpg"
+        profileSrc="https://media.istockphoto.com/photos/smiling-mixed-race-mature-man-on-grey-background-picture-id1319763895?b=1&k=20&m=1319763895&s=170667a&w=0&h=jhqKyg62cQVZ-NjDQjpcenCdHDrprkN4caRjk4K76E8="
+        title="Kekarot"
+      />
+    </div>
+  );
+}
+
+export default StoryReel;
